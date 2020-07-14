f50b68118bbed1d0ab3830632db95e924e7a31a2
Sébastien Ros <sebastienros@gmail.com>
2020-07-14 13:05:35 -0700

    Adding Context argument (#10)

---
+++ src/Shortcodes/Context.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace Shortcodes
+{
+    public class Context : IEnumerable<KeyValuePair<string, object>>
+    {
+        private Dictionary<string, object> _items;
+
+        private Dictionary<string, object> Items => _items ??= new Dictionary<string, object>();
+
+        public object this[string key]
+        {
+            get
+            {
+                return Items[key];
+            }
+            set
+            {
+                Items[key] = value;
+            }
+        }
+
+        public ICollection<string> Keys => Items.Keys;
+
+        public int Count => Items.Count;
+
+        public void Clear()
+        {
+            Items.Clear();
+        }
+
+        public bool ContainsKey(string key)
+        {
+            return Items.ContainsKey(key);
+        }
+
+        public IEnumerator<KeyValuePair<string, object>> GetEnumerator()
+        {
+            return Items.GetEnumerator();
+        }
+
+        public bool Remove(string key)
+        {
+            return Items.Remove(key);
+        }
+
+        public bool TryGetValue(string key, out object value)
+        {
+            return Items.TryGetValue(key, out value);
+        }
+
+        public T GetOrSetValue<T>(string key, T value)
+        {
+            if (Items.TryGetValue(key, out var result))
+            {
+                if (result is T t)
+                {
+                    return t;
+                }
+                else
+                {
+                    return default(T);
+                }
+            }
+            else
+            {
+                Items[key] = value;
+
+                return value;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return Items.GetEnumerator();
+        }
+    }
+}
--- src/Shortcodes/IShortcodeProvider.cs
+++ src/Shortcodes/IShortcodeProvider.cs
@@ -2,10 +2,25 @@
 
 namespace Shortcodes
 {
-    public delegate ValueTask<string> ShortcodeDelegate(Arguments arguments, string content);
+    /// <summary>
+    /// Delegate evaluated when a shortcode has matched.
+    /// </summary>
+    /// <param name="arguments">The arguments passed with the shortcode.</param>
+    /// <param name="content">The inner content of the shortcode.<code>null</code> for self-closing shortcodes.</param>
+    /// <param name="context">Custom properties used to evaluate a template.</param>
+    /// <returns>The string to substitue the shortcode with.</returns>
+    public delegate ValueTask<string> ShortcodeDelegate(Arguments arguments, string content, Context context);
 
     public interface IShortcodeProvider
     {
-        ValueTask<string> EvaluateAsync(string identifier, Arguments arguments, string content);
+        /// <summary>
+        /// Evaluates a named shortcode.
+        /// </summary>
+        /// <param name="identifier">The name of the shortcode to evaluate.</param>
+        /// <param name="arguments">The arguments passed with the shortcode.</param>
+        /// <param name="content">The inner content of the shortcode.<code>null</code> for self-closing shortcodes.</param>
+        /// <param name="context">Custom properties used to evaluate a template.</param>
+        /// <returns>The string to substitue the shortcode with.</returns>
+        ValueTask<string> EvaluateAsync(string identifier, Arguments arguments, string content, Context context);
     }
 }
