a31ab09dbe9ea231108312b3c29cf88046db6d3f
Sebastien Ros <sebastienros@gmail.com>
2020-12-11 15:14:11 -0800

    Adding benchmarks

---
+++ tests/Shortcodes.Benchmarks/RenderBenchmarks.cs
@@ -0,0 +1,32 @@
+﻿using BenchmarkDotNet.Attributes;
+using System.Threading.Tasks;
+
+namespace Shortcodes.Benchmarks
+{
+    public class RenderBenchmarks
+    {
+        private NamedShortcodeProvider _provider;
+
+        private ShortcodesProcessor _processor;
+
+        public RenderBenchmarks()
+        {
+            _provider = new NamedShortcodeProvider
+            {
+                ["upper"] = (args, content, ctx) => new ValueTask<string>(content.ToUpperInvariant()),
+            };
+
+            _processor = new ShortcodesProcessor(_provider);
+        }
+
+        [Benchmark]
+        public async Task<string> Nop() => await _processor.EvaluateAsync("Lorem ipsum dolor est");
+
+        [Benchmark]
+        public async Task<string> Upper() => await _processor.EvaluateAsync("Lorem [upper]ipsum[/upper] dolor est");
+
+        [Benchmark]
+        public async Task<string> Unkown() => await _processor.EvaluateAsync("Lorem [lower]ipsum[/lower] dolor est");
+
+    }
+}
